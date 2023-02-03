74032e8e6c6a9d2ca7295511c1ca5e7320e7b5b5
Ronith Thodimeladinne <rthodime@uci.edu>
2023-02-03 12:06:09 -0800

    Switch to a+ but keep seek

--- tokenizer/__init__.py
+++ tokenizer/__init__.py
@@ -97,13 +97,13 @@ def checkSimilarity(resp):
         hashes = generateHashes(freqs)
         final_hash = getFinalHash(freqs, hashes)
 
-        with open(HASH_FILE_NAME, "w+", buffering=1) as f:
+        with open(HASH_FILE_NAME, "a+", buffering=1) as f:
             f.seek(0, 0)
             for line in f.readlines():
                 # if line is not None and hashes similar, return True
                 if line is not None and compareHash(final_hash, line.strip()):
                     return True
-
+            
             # no similar hashes found, write hash to file
             f.write(f"{final_hash}\n")
             return False
