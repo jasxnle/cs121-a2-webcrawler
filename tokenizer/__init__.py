9e6a47d9cb8ba4c9581e5f56c8a53b2cc24d850a
Jason Le <jasonle2020@gmail.com>
2023-02-03 16:54:15 -0800

    added stopwords to tokenize function

--- tokenizer/__init__.py
+++ tokenizer/__init__.py
@@ -1,16 +1,15 @@
 import re
 import hashlib
 from bs4 import BeautifulSoup       # FIXME: extract all soup references to worker.py
-from nltk.corpus import stopwords
 # List<Token> tokenize(TextFile)
 
 # This functions runs in O(nm) due to check each line in the file at a time
 # and then iterating through m words in the list
+# Assumption tokenizer does not handle contractions
 def tokenize(text):
     tokens = re.findall(r"[a-zA-Z0-9]+", text.lower())
-    stop_words = set(stopwords.words('english'))
-    stop_words = [i.lower() for i in stop_words]
-    return [i.lower() for i in tokens if i.lower() not in stop_words]
+    stop_words = set([ 'a','about','above','after','again','against','all','am','an','and','any','are',"aren't","as","at","be","because","been","before","being","below","between","both","but","by","can't","cannot","could","couldn't","did","didn't","do","does","doesn't","doing","don't","down","during","each","few","for","from","further","had","hadn't","has","hasn't","have","haven't","having","he","he'd","he'll","he's","her","here","here's","hers","herself","him","himself","his","how","how's","i","i'd","i'll","i'm","i've","if","in","into","is","isn't","it","it's","its","itself","let's","me","more","most","mustn't","my","myself","no","nor","not","of","off","on","once","only","or","other","ought","our","ours","ourselves","out","over","own","same","shan't","she","she'd","she'll","she's","should","shouldn't","so","some","such","than","that","that's","the","their","theirs","them","themselves","then","there","there's","these","they","they'd","they'll","they're","they've","this","those","through","to","too","under","until","up","very","was","wasn't","we","we'd","we'll","we're","we've","were","weren't","what","what's","when","when's","where","where's","which","while","who","who's","whom","why","why's","with","won't","would","wouldn't","you","you'd","you'll","you're","you've","your","yours","yourself","yourselves"])
+    return [i.lower() for i in tokens if i.lower() not in stop_words and len(i.lower()) >= 3 ]
     #exception handling
 
 
