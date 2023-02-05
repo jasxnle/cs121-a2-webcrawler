7321aa8b56bcbc5e64cfb6efcfe11899f885c4c0
Ronith Thodimeladinne <rthodime@uci.edu>
2023-02-04 22:30:58 -0800

    Add check in isValid for detecting pdf in queries

--- scraper.py
+++ scraper.py
@@ -139,6 +139,9 @@ def is_valid(url):
         
         if re.search(r"share=", parsed.query.lower()):
             return False
+        
+        if re.search(r".*\=pdf", parsed.query.lower()):
+            return False
         # check if link is broken
         #
         return not re.match(
