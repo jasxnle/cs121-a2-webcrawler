93524445262fe224268c70295716188cd0299d34
Jason Le <jasonle2020@gmail.com>
2023-02-09 19:18:45 -0800

    Release web crawler 1.0.2 - Added comments to code and revised

--- scraper.py
+++ scraper.py
@@ -4,8 +4,19 @@ from urllib.parse import urljoin
 from bs4 import BeautifulSoup
 from tokenizer import tokenize, computeWordFrequencies, mergeDictionary
 
-#
+
 def checkSubdomain(url, resp):
+    """This function checks if a url has a valid subdomain
+
+    Args:
+        url : Url that we want to check if it has a sub domain
+        resp : response object containing status code and page content
+
+    Returns:
+        tuple : (True or False if a subdomain is present, full url with subdomain)
+
+    """
+    #check if responses is valid to process
     if resp.status != 200 or resp is None or resp.raw_response is None or resp.raw_response.content is None :
         return (False, None)
 
@@ -21,6 +32,16 @@ def checkSubdomain(url, resp):
 
 
 def getLengthOfResponseContent(resp):
+    """This function finds the largest web page crawled
+
+    Args:
+        resp : response object that contains stauts code and content of a url
+
+    Returns:
+        int : returns length for the response content
+
+    """
+    #check if responses is valid to process
     if resp.status != 200 or resp is None or resp.raw_response is None or resp.raw_response.content is None :
         return 0
 
@@ -29,13 +50,35 @@ def getLengthOfResponseContent(resp):
 
 # Returns all the common words
 def tokenizeResponseContent(resp, words):
+    """this function is tokenizes the content of a webpage
+
+    Args:
+        resp : response object that contains stauts code and content of a url
+        words : dictionary of current common words
+
+    Returns:
+        dictionary : merged diction of common words 
+
+    """
+    #check is resp is a valid to process
     if resp.status != 200 or resp is None or resp.raw_response is None or resp.raw_response.content is None :
         return words
 
     soup = BeautifulSoup(resp.raw_response.content, "html.parser")
     return mergeDictionary(computeWordFrequencies(tokenize(soup.get_text())), words)
 
+
 def scraper(url, resp):
+    """This function is used to scrape the webpage
+
+    Args:
+        url : Url that we want to check if it has a sub domain
+        resp : response object containing status code and page content
+
+    Returns:
+        list : next links to crawl that are valid
+
+    """
     links = extract_next_links(url, resp)
     # use starting url and response to grab next links and data
     # create structure to store all links to visit
@@ -44,6 +87,16 @@ def scraper(url, resp):
     return [link for link in links if is_valid(link)]
 
 def extract_next_links(url, resp):
+    """Getting next links from a url
+
+    Args:
+        url : Url that we want to check if it has a sub domain
+        resp : response object containing status code and page content
+
+    Returns:
+        list : next links to crawl
+
+    """
     # url: the URL that was used to get the page
     # resp.url: the actual url of the page
     # resp.status: the status code returned by the server. 200 is OK, you got the page. Other numbers mean that there was some kind of problem.
@@ -75,8 +128,7 @@ def extract_next_links(url, resp):
 
         return links
 
-    #FIXME
-    #Add to config file later
+    #If file size is too large above threshold 4GB
     if len(resp.raw_response.content) > 4 * 1e9:
         return list()
 
@@ -88,13 +140,13 @@ def extract_next_links(url, resp):
         return links
 
     a_tags = soup.find_all("a")
-
+    #begin the scraping 
 
     for a in a_tags:
         link = a.get("href")
 
-        #if link is empty
-        if(not link):
+        #if link is empty or is None
+        if(not link or link is None):
             continue
 
         link = link.strip()
@@ -103,12 +155,10 @@ def extract_next_links(url, resp):
         if (bool(urlparse(link).netloc) == False):
             if (link is not None and link != "" and link[0] != "/"):
                 link = "/" + link
-            # FIXME: url = "https://www.ics.uci.edu/~vazirani", link = "/WNT Algorytmy aproksymacyjne.htm"
-            #        link = "https://www.ics.uci.edu/WNT Algorytmy aproksymacyjne.htm"
-            #        Possibly just append?
-            # FIXME: urljoin does not handle "~" well because it is not in the URL standard
             link = urljoin(url, link)
 
+        # if is relative link, correct faulty relative links
+        # e.g. `href = "internal/path"` => `href = "/internal/path"`
         if (bool(urlparse(link).fragment)):
             link = link.split('#')[0]
 
@@ -119,6 +169,15 @@ def extract_next_links(url, resp):
     return links
 
 def is_valid(url):
+    """checks is a url is a valid url to crawl
+
+    Args:
+        url : url that we are currently trying to crawl
+
+    Returns:
+        boolean : true if need to crawl, false not to crawl
+
+    """
     # Decide whether to crawl this url or not.
     # If you decide to crawl it, return True; otherwise return False.
     # There are already some conditions that return False.
@@ -144,6 +203,7 @@ def is_valid(url):
         # if re.match(r".*(calendar|swiki|wiki).*", parsed.hostname):
         #     return False
 
+        # These if statements continue to filter out links that we do not want
         if re.search(r"/(pdf|wp-json)/", parsed.path.lower()):
             return False
 
@@ -153,8 +213,6 @@ def is_valid(url):
         if re.search(r".*\=pdf", parsed.query.lower()):
             return False
 
-        # check if link is broken
-        #
         return not re.match(
             r".*\.(css|js|bmp|gif|jpe?g|ico"
             + r"|png|tiff?|mid|mp2|mp3|mp4"
--- tokenizer/__init__.py
+++ tokenizer/__init__.py
@@ -2,20 +2,39 @@ import re
 import hashlib
 from bs4 import BeautifulSoup       # FIXME: extract all soup references to worker.py
 # List<Token> tokenize(TextFile)
-
-# This functions runs in O(nm) due to check each line in the file at a time
-# and then iterating through m words in the list
-# Assumption tokenizer does not handle contractions
 def tokenize(text):
+    """Would be O(N*M), where N is the number of lines and M is the number of tokens within
+    the line. Since there is technically a nested for loop within the program for iterating through
+    the file and the list comprehension for every token, this would have to lead to a quadratic time complexity
+
+    Assumption: tokenizer does not handle contractions
+
+    Args:
+        text: str, extracted str from HTML content i.e. no tags
+
+    Returns:
+        List<Token>: list of tokens parsed from HTML content, includes duplicates
+
+    """
     tokens = re.findall(r"[a-zA-Z0-9]+", text.lower())
     stop_words = set([ 'a','about','above','after','again','against','all','am','an','and','any','are',"aren't","as","at","be","because","been","before","being","below","between","both","but","by","can't","cannot","could","couldn't","did","didn't","do","does","doesn't","doing","don't","down","during","each","few","for","from","further","had","hadn't","has","hasn't","have","haven't","having","he","he'd","he'll","he's","her","here","here's","hers","herself","him","himself","his","how","how's","i","i'd","i'll","i'm","i've","if","in","into","is","isn't","it","it's","its","itself","let's","me","more","most","mustn't","my","myself","no","nor","not","of","off","on","once","only","or","other","ought","our","ours","ourselves","out","over","own","same","shan't","she","she'd","she'll","she's","should","shouldn't","so","some","such","than","that","that's","the","their","theirs","them","themselves","then","there","there's","these","they","they'd","they'll","they're","they've","this","those","through","to","too","under","until","up","very","was","wasn't","we","we'd","we'll","we're","we've","were","weren't","what","what's","when","when's","where","where's","which","while","who","who's","whom","why","why's","with","won't","would","wouldn't","you","you'd","you'll","you're","you've","your","yours","yourself","yourselves"])
     return [i.lower() for i in tokens if i.lower() not in stop_words and len(i.lower()) >= 3 ]
     #exception handling
 
 
-# Map<token,count> computeWordFrequencies(List<Token>)
-# This function is O(n) because we are iterating through the array tokens which has a length of n.
 def computeWordFrequencies(tokens):
+    """From a list of tokens, create a dictionary that represents their frequency
+    This function is O(n) because we are iterating through the array tokens which has a length of n.
+
+    Args:
+        tokens: List<str>, value from tokenize(text)
+
+    Returns:
+        Map<token, count>: dictionary of each token provided from tokens and the
+                           frequency it shows up
+
+    """
+
     freq = {}
 
     #adding to hashmap
@@ -27,9 +46,22 @@ def computeWordFrequencies(tokens):
             freq[i] +=1
     return freq
 
-# This function should have a time complexity of O(nm + min(s, p)) since to tokenize each file it takes O(nm) time.
-# Then finding the intersection of the two tokens for each file will take O(min(s,p)) due to having to iterate through at most all elements in the smaller array
 def findCommonTokens(text1, text2):
+    """Finds common tokens between two different text strings using tokenize() function
+
+    This function should have a time complexity of O(nm + min(s, p)) since to
+    tokenize each file it takes O(nm) time. Then finding the intersection
+    of the two tokens for each file will take O(min(s,p)) due to having to
+    iterate through at most all elements in the smaller array
+
+    Args:
+        text1: extracted str from HTML content i.e. no tags
+        text2: extracted str from HTML content i.e. no tags
+
+    Returns:
+        List<token>: list of tokens shared by both text1 and text2
+
+    """
     #tokens from each file
     tok1 = tokenize(text1)
     tok2 = tokenize(text2)
@@ -41,6 +73,18 @@ def findCommonTokens(text1, text2):
     return commonTok
 
 def mergeDictionary(d1, d2):
+    """Merges two defaultdict(int)
+    Meant to merge two dictionaries from result of computeWordFrequencies
+
+    Args:
+        d1: Map<token, count> from computeWordFrequencies
+        d2: Map<token, count> from computeWordFrequencies
+
+    Returns:
+        defaultdict(int): Merged defaultdict(int) combining the frequencies of
+                          both dictionaries
+
+    """
     d3 = {**d1, **d2}
     for key, value in d3.items():
         if key in d1 and key in d2:
@@ -49,6 +93,15 @@ def mergeDictionary(d1, d2):
 
 # tokens: takes result from computeWordFrequencies
 def generateHashes(tokens):
+    """Generate unique hash for each token using SimHash algorithm with md5 algo.
+
+    Args:
+        tokens: Map<token, count> result from computeWordFrequencies
+
+    Returns:
+        Map<token, hash>: result used in getFinalHash
+
+    """
     res = {}
     for token in tokens:
         hex_to_int = int(hashlib.md5(token.encode()).hexdigest(), 16)
@@ -58,6 +111,16 @@ def generateHashes(tokens):
 # freqs: result from computeWordFrequencies
 # hashes: result from generateHashes
 def getFinalHash(freqs, hashes):
+    """Final unique hash used to compare two hashes to test similarity
+
+    Args:
+        freqs: Map<token, count>, result from computeWordFrequencies
+        hashes: Map<token, hash>, result from generateHashes
+
+    Returns:
+        str: final hash_str
+        
+    """
     length = 128
     final_hash = [0]*length
     for i in range(length):
@@ -79,6 +142,17 @@ def getFinalHash(freqs, hashes):
     return hash_str
 
 def compareHash(hash1, hash2):
+    """Compare hashes from getFinalHash, determines if similar by SIMILARITY_THRESHOLD
+
+    Args:
+        hash1: str, result from getFinalHash
+        hash2: str, result from getFinalHash
+
+    Returns:
+        bool: if hashes are within SIMILARITY_THRESHOLD
+
+    """
+
     SIMILARITY_THRESHOLD = 0.8
     length = 128
     numMatches = 0
@@ -91,6 +165,17 @@ def compareHash(hash1, hash2):
     return False
 
 def checkSimilarity(resp):
+    """Determine if incoming URL's response is similar to any of the unique hashes
+    stored in HASH_FILE_NAME.
+
+    Args:
+        resp: response to check if similar to other responses
+
+    Returns:
+        bool: returns True if incoming response is similar, return False and write
+              new unique hash to file otherwise
+
+    """
     HASH_FILE_NAME = "HASH_FILE.txt"         # FIXME: choose different name per worker to make thread-safe
     # FIXME: what to return when resp is empty
     if resp is not None and resp.raw_response is not None and resp.raw_response.content is not None and resp.raw_response.content != "":
